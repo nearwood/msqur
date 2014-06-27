c71e499ee8684843d26ad9a1224df41a69d20fa3
nearwood <nearwood@localhost>
2014-06-27 14:19:16 -0400

    f

--- app.js
+++ app.js
@@ -1,2 +1,1 @@
-var app = angular.module('MSQApp',[]);
-
+var app = angular.module('TuneShare',[]);
---
+++ uploadmanager.js
@@ -0,0 +1,28 @@
+app.factory('uploadManager', function ($rootScope) {
+    var _files = [];
+    return {
+        add: function (file) {
+            _files.push(file);
+            $rootScope.$broadcast('fileAdded', file.files[0].name);
+        },
+        clear: function () {
+            _files = [];
+        },
+        files: function () {
+            var fileNames = [];
+            $.each(_files, function (index, file) {
+                fileNames.push(file.files[0].name);
+            });
+            return fileNames;
+        },
+        upload: function () {
+            $.each(_files, function (index, file) {
+                file.submit();
+            });
+            this.clear();
+        },
+        setProgress: function (percentage) {
+            $rootScope.$broadcast('uploadProgress', percentage);
+        }
+    };
+});
