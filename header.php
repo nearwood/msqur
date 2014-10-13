e2244bc00777986f7bb7a941269246cf10659fe3
nearwood <nearwood@localhost>
2014-10-13 12:32:22 -0400

    upload front good enough

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>TuneShare</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="msqur.css" />
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/themes/smoothness/jquery-ui.css" />
+	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script>
+	<script src="msqur.js"></script>
+</head>
+<body>
+<div id="navigation"><button id="btnUpload">Upload</button></div>
+<div id="upload" style="display:none;">
+	<div id="fileDropZone">Drop files here
+		<input type="file" id="fileSelect" name="files[]" multiple />
+	</div>
+	<output id="fileList"></output>
+</div>
---
+++ msqur.js
@@ -0,0 +1,59 @@
+$(function() {
+	
+	$('div#upload').dialog({
+		modal: true,
+		autoOpen: false,
+		title: "Upload Tune Files",
+		width: "450px",
+		buttons: {
+			Upload: function() {},
+			Cancel: function() { $(this).dialog('close'); }
+		}
+	});
+	
+	
+	$('#btnUpload').click(function(e) {
+		if (window.File && window.FileReader && window.FileList && window.Blob)
+		{
+			$('div#upload').dialog('open');
+		} else {
+			alert('The File APIs are not fully supported in this browser.');
+			//TODO no ajax file upload
+		}
+	});
+	
+	function uploadAdd(e)
+	{
+		e.stopPropagation();
+		e.preventDefault();
+		
+		var files = e.target.files || e.dataTransfer.files
+		//TODO type check
+		var output = [];
+		for (var i = 0, f; f = files[i]; ++i)
+		{
+			output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+			f.size, ' bytes, last modified: ',
+			f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+			'</li>');
+		}
+		$('output#fileList').html('<ul>' + output.join('') + '</ul>');
+	}
+	
+	function uploadDragOver(e)
+	{
+		e.stopPropagation();
+		e.preventDefault();
+		e.dataTransfer.dropEffect = 'copy';
+	}
+	
+	function upload(files)
+	{
+		
+	}
+	
+	$('input#fileSelect').change(uploadAdd);
+	var dropZone = document.getElementById('fileDropZone');
+	dropZone.addEventListener('dragover', uploadDragOver);
+	dropZone.addEventListener('drop', uploadAdd);
+});
