f1fb4abf1313838e6fe3b37b0e7813ea0daa21fc
nearwood <nearwood@localhost>
2014-12-22 14:36:57 -0500

    Update DB

--- msqur.sql
+++ msqur.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.2
+-- version 4.3.3
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 18, 2014 at 01:44 PM
+-- Generation Time: Dec 22, 2014 at 02:36 PM
 -- Server version: 10.0.15-MariaDB-log
 -- PHP Version: 5.6.3
 
@@ -19,22 +19,24 @@ SET time_zone = "+00:00";
 --
 -- Database: `msqur`
 --
+CREATE DATABASE IF NOT EXISTS `msqur` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `msqur`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `engine`
+-- Table structure for table `engines`
 --
 
-DROP TABLE IF EXISTS `engine`;
-CREATE TABLE IF NOT EXISTS `engine` (
+DROP TABLE IF EXISTS `engines`;
+CREATE TABLE IF NOT EXISTS `engines` (
   `id` int(11) NOT NULL,
-  `displacement` int(11) NOT NULL,
-  `numCylinders` int(11) NOT NULL,
-  `compression` int(11) NOT NULL,
+  `displacement` decimal(4,2) NOT NULL,
+  `numCylinders` tinyint(2) NOT NULL,
+  `compression` decimal(4,2) NOT NULL,
   `induction` int(11) NOT NULL,
   `injectorSize` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -46,7 +48,7 @@ DROP TABLE IF EXISTS `metadata`;
 CREATE TABLE IF NOT EXISTS `metadata` (
   `id` int(11) NOT NULL,
   `msq` int(11) NOT NULL,
-  `engine` int(11) NOT NULL,
+  `engine` int(11) DEFAULT NULL,
   `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `fileFormat` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `signature` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -55,7 +57,7 @@ CREATE TABLE IF NOT EXISTS `metadata` (
   `writeDate` datetime NOT NULL,
   `uploadDate` datetime NOT NULL,
   `tuneComment` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -67,16 +69,16 @@ DROP TABLE IF EXISTS `msqs`;
 CREATE TABLE IF NOT EXISTS `msqs` (
   `id` int(11) NOT NULL,
   `xml` mediumtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `engine`
+-- Indexes for table `engines`
 --
-ALTER TABLE `engine`
+ALTER TABLE `engines`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -96,20 +98,20 @@ ALTER TABLE `msqs`
 --
 
 --
--- AUTO_INCREMENT for table `engine`
+-- AUTO_INCREMENT for table `engines`
 --
-ALTER TABLE `engine`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `engines`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `metadata`
 --
 ALTER TABLE `metadata`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `msqs`
 --
 ALTER TABLE `msqs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
