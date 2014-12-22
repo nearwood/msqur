7a0ddaf441f08af0fafc2405f5a7785f605c6c43
nearwood <nearwood@localhost>
2014-12-22 14:54:27 -0500

    Add stats. Fix DB export file.

--- admin.php
+++ admin.php
@@ -1,12 +1,7 @@
 <?php
-//trim/concentrate/whatever engines (remove dupes)
-//remove all msqs/metadata/engines (fresh slate)
-
-//find metada and engines (browse)
-SELECT * FROM `metadata` JOIN `engines` ON engines.id = metadata.engine 
-
-//DELETE ALL DATA
-TRUNCATE metadata
-TRUNCATE engines
-TRUNCATE msqs
+//total msqs
+//total something
+//total views
+//most popular
+//most viewed
 ?>
