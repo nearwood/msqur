b5a9d042967898b34ca98ccaa048771b4f82350c
nearwood <nearwood@localhost>
2014-12-22 13:57:07 -0500

    Added basic browse. Added non-working search.php and admin.php. Finalized engine DB structure. Added upload engine form. Added non-working browse filter.

---
+++ admin.php
@@ -0,0 +1,4 @@
+<?php
+//trim/concentrate/whatever engines (remove dupes)
+//remove all msqs/metadata/engines (fresh slate)
+?>
--- browse.php
+++ browse.php
@@ -1,31 +1,40 @@
 <?php
 
 require('db.php');
-
 require('header.php');
 
 ?>
-Latest Additions
-Most Viewed
-
-Cylinders
-Liters
-Injector Size
-Forced Induction | NA
-Cams
-Headwork
 <div id='content'>
+<div>
+<form id="filter">
+	<fieldset>
+	<legend>Engine Filter</legend>
+	<div>Cylinders: <input type="number" min="0" value="6" max="99" style="width:3em;"/></div>
+	<div>Displacement (liters): <input type="number" min="0" step="0.01" value="3.0" style="width:4em;"/> +/- <span id="literMargin">0%<input type="number" min="0" step="1"></span></div>
+	<div>Compression (X:1) <input name="compression" type="number" min="0" step="0.1" value="9.0" style="width:4em;"/> +/- <span id="literMargin">0%<input type="number" min="0" step="1"></span></div>
+	<div>Aspiration: 
+		<select>
+			<option value="na" title="AKA: Slow">Naturally Aspirated</option>
+			<option value="fi" title="The way God intended">Forced Induction</option>
+		</select>
+	</div>
+	<div>Firmware: X</div>
+	<div><button>Refresh</button></div>
+	</fieldset>
+</form>
+</div>
 <?php
 $results = getAll();
 $numResults = count($results);
 //echo '<div class="debug">' . var_dump($results) . '</div>';
 echo '<div class="info">' . $numResults . ' results.</div>';
-echo '<ul>';
+echo '<table>';
+echo '<tr><th>ID</th><th>Cylinders</th><th>Liters</th><th>NA</th><th>Firmware/Version</th><th>Upload Date</th><th>Views</th></th>';
 for ($c = 0; $c < $numResults; $c++)
 {
-	echo '<li>' . $results[$c]['id'] . ' - ' . $results[$c]['id'] . '</li>';
+	echo '<tr><td><a href="index.php?msq=' . $results[$c]['id'] . '">MSQ #' . $results[$c]['id'] . '</a></td><td>X</td><td>Y</td><td>Z</td><td>a.b</td><td>' . $results[$c]['uploadDate'] . '</td><td></td></tr>';
 }
-echo '</ul>';
+echo '</table>';
 ?>
 </div>
 <?php require('footer.php'); ?>
