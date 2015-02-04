c133e3d7e62ca1c4c3466de01ba653681818fb98
nearwood <nearwood@localhost>
2015-02-04 15:26:32 -0500

    Add 2D Table caps, but it's rough. INI file parsing mostly done. Need more 1D constant formatting.

--- msq.format.php
+++ msq.format.php
@@ -1,17 +1,25 @@
 <?php
 //MSQ <=> INI <=> FE links/lookups
 
-function getLookup()
+function getSchema()
 {
 	return array(// XML/INI Name => pretty name, [xAxisXmlName, yAxisXmlName]
 		'veTable' => array('name' => 'VE Table', 'x' => 'frpm_table', 'y' => 'fmap_table', 'units' => '%', 'hot' => 'descending'),
 		'veTable1' => array('name' => 'VE Table 1', 'x' => 'frpm_table1', 'y' => 'fmap_table1', 'units' => '%', 'hot' => 'descending'),
 		'veTable2' => array('name' => 'VE Table 2', 'x' => 'frpm_table2', 'y' => 'fmap_table2', 'units' => '%', 'hot' => 'descending'),
-		'advanceTable1' => array('name' => 'Timing Advance', 'x' => 'srpm_table1', 'y' => 'smap_table1', 'units' => 'degrees', 'hot' => 'ascending'),
-		'advanceTable2' => array('name' => 'Timing Advance', 'x' => 'srpm_table2', 'y' => 'smap_table2', 'units' => 'degrees', 'hot' => 'ascending'),
-		'afrTable1' => array('name' => 'AFR Targets', 'x' => 'arpm_table1', 'y' => 'amap_table1', 'hot' => 'ascending'),
+		'advanceTable1' => array('name' => 'Timing Advance 1', 'x' => 'srpm_table1', 'y' => 'smap_table1', 'units' => 'degrees', 'hot' => 'ascending'),
+		'advanceTable2' => array('name' => 'Timing Advance 2', 'x' => 'srpm_table2', 'y' => 'smap_table2', 'units' => 'degrees', 'hot' => 'ascending'),
+		'afrTable1' => array('name' => 'AFR Targets 1', 'x' => 'arpm_table1', 'y' => 'amap_table1', 'hot' => 'ascending'),
+		'afrTable2' => array('name' => 'AFR Targets 2', 'x' => 'arpm_table2', 'y' => 'amap_table2', 'hot' => 'ascending'),
+		'nCylinders' => array('name' => 'Cylinders'),
+		'engineType' => array('name' => 'Engine Type'),
+		'reqFuel' => array('name' => 'Base Fuel (reqFuel)'),
+		'twoStroke' => array('name' => '# Strokes'),
+		'injType' => array('name' => 'Injection'),
+		'nInjectors' => array('name' => 'Injectors'),
 		'egoType' => array('name' => 'O2 Sensor Type'),
-		'nCylinders' => array('name' => 'Cylinders')
+		'crankingRPM' => array('name' => 'Cranking RPM Threshold'),
+		'dwellcorr' => array('name' => 'Dwell Voltage Correction', 'y' => 'dwellvolts', 'units' => '%', 'hot' => 'ascending')
 	);
 }
 
