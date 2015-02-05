d4963e644a1a940ba2ef799d80e01a38c1d755ec
nearwood <nearwood@localhost>
2015-02-05 14:24:17 -0500

    Refactor msq parsing into separate functions. Also parse and update metadata (firmware sig, etc.) and engine data. Fix MS3 ini files. Fix ini file filename algorithm. Add some better error handling and stuff.

--- parse.ini.php
+++ parse.ini.php
@@ -1,18 +1,20 @@
-<html>
-<body>
 <?php
-
-//~ $result = parse_ms_ini("ini/test.ini", TRUE);
-//~ print "<pre>";
-//~ var_export($result);
-//~ print "</pre>";
 
 //goulven.ch@gmail.com (php.net comments) http://php.net/manual/en/function.parse-ini-file.php#78815
 function parse_ms_ini($file, $something)
 {
 	if (DEBUG) echo "<div class=\"debug\">Attempting to open: $file</div>";
-	$ini = file($file, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
-	if (count($ini) == 0) return array();
+	try
+	{
+		$ini = file($file, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
+	}
+	catch (Exception $e)
+	{
+		echo "<div class=\"error\">Could not open: $file</div>";
+		return null;
+	}
+	
+	if ($ini == null || count($ini) == 0) return null;
 	else if (DEBUG) echo "<div class=\"debug\">File opened.</div>";
 	
 	$globals = array();
@@ -74,33 +76,4 @@ function parse_ms_ini($file, $something)
 	return $result + $globals;
 }
 
-
-/*
-
- * INTERESTING SETTINGS:
-settingOption = MICROSQUIRT_MODULE, "MicroSquirt Module, MSPNP/DIYPNP"
-indicator = { crank              }, "Not Cranking", "Cranking",    white, black, green,   black
-
-
-[MegaTune]
-   MTversion      = 2.25 ; MegaTune itself; needs to match exec version.
-
-   queryCommand   = "Q" ; B&G embedded code version 2.0/2.98x/3.00
-   signature      = 20  ; Versions above return a single byte, 20T.
-
-;-------------------------------------------------------------------------------
- page = 1
-   ;  name       = bits,   type, offset, bits
-   ;  name       = array,  type, offset, shape, units,     scale, translate,    lo,      hi, digits
-   ;  name       = scalar, type, offset,        units,     scale, translate,    lo,      hi, digits
-	  veTable    = array,  U08,       0, [8x8], "%",          1.0,      0.0,   0.0,   255.0,      0
-	  crankCold  = scalar, U08,      64,         "ms",       0.1,       0.0,   0.0,    25.5,      1
-#if CELSIUS
-	  egoTemp    = scalar, U08,      86,         "°C",     0.555,       -72,   -40,   102.0,      0
-#else
-	  egoTemp    = scalar, U08,      86,         "°F",       1.0,       -40,   -40,   215.0,      0
-#endif
-*/
 ?>
-</body>
-</html>
