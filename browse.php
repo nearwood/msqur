7b7c7cb810f40065fd779c7059c0ffd8f608ffdc
nearwood <nearwood@gmail.com>
2015-02-08 16:50:51 -0500

    merge.

--- browse.php
+++ browse.php
@@ -34,7 +34,7 @@ echo '<table>';
 echo '<tr><th>ID</th><th>Cylinders</th><th>Liters</th><th>Compression</th><th>Turbo</th><th>Firmware/Version</th><th>Upload Date</th><th>Views</th></th>';
 for ($c = 0; $c < $numResults; $c++)
 {
-	echo '<tr><td><a href="index.php?msq=' . $results[$c]['id'] . '">' . $results[$c]['id'] . '</a></td><td>' . $results[$c]['numCylinders'] . '</td><td>' . $results[$c]['displacement'] . '</td><td>' . $results[$c]['compression'] . ':1</td><td>' . $results[$c]['induction'] . '</td><td>' . $results[$c]['firmware'] . '/' . $results[$c]['signature'] . '</td><td>' . $results[$c]['uploadDate'] . '</td><td></td></tr>';
+	echo '<tr><td><a href="index.php?msq=' . $results[$c]['mid'] . '">' . $results[$c]['mid'] . '</a></td><td>' . $results[$c]['numCylinders'] . '</td><td>' . $results[$c]['displacement'] . '</td><td>' . $results[$c]['compression'] . ':1</td><td>' . $results[$c]['induction'] . '</td><td>' . $results[$c]['firmware'] . '/' . $results[$c]['signature'] . '</td><td>' . $results[$c]['uploadDate'] . '</td><td></td></tr>';
 }
 echo '</table>';
 ?>
