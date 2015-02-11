3e101bea8125a84a319c0b915a817181d381c36b
nearwood <nearwood@localhost>
2015-02-11 17:44:42 -0500

    Add doxygen.wq

---
+++ doxygen/html/navtreedata.js
@@ -0,0 +1,14 @@
+var NAVTREE =
+[
+  [ "msqur", "index.html", [
+    [ "MSQur", "md__srv_http_msqur__r_e_a_d_m_e.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"index.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
