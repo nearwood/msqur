c528ae58c4d11607e078a8551899d516c329be63
nearwood <nearwood@gmail.com>
2015-02-15 22:02:15 -0500

    Progress on OO for #14.

--- src/browse.php
+++ src/browse.php
@@ -1,24 +1,25 @@
-<div id='content'>
 <?php
 require "msqur.php";
 
 if (isset($_GET['p'])) {
 	$page = $_GET['p']; //TODO processing
-}
+} else $page = 0;
 
-$msqur->browse($page);
-//$results = browseAll();
-//$numResults = count($results);
+$results = $msqur->browse($page);
+$numResults = count($results);
 
 //echo '<div class="debug">'; var_export($results); echo '</div>';
 
-echo '<div class="info">' . $numResults . ' results.</div>';
+$msqur->header();
+
+echo '<div id="content"><div class="info">' . $numResults . ' results.</div>';
 echo '<table>';
 echo '<tr><th>ID</th><th>Cylinders</th><th>Liters</th><th>Compression</th><th>Turbo</th><th>Firmware/Version</th><th>Upload Date</th><th>Views</th></th>';
 for ($c = 0; $c < $numResults; $c++)
 {
-	echo '<tr><td><a href="index.php?msq=' . $results[$c]['mid'] . '">' . $results[$c]['mid'] . '</a></td><td>' . $results[$c]['numCylinders'] . '</td><td>' . $results[$c]['displacement'] . '</td><td>' . $results[$c]['compression'] . ':1</td><td>' . $results[$c]['induction'] . '</td><td>' . $results[$c]['firmware'] . '/' . $results[$c]['signature'] . '</td><td>' . $results[$c]['uploadDate'] . '</td><td></td></tr>';
+	echo '<tr><td><a href="view.php?msq=' . $results[$c]['mid'] . '">' . $results[$c]['mid'] . '</a></td><td>' . $results[$c]['numCylinders'] . '</td><td>' . $results[$c]['displacement'] . '</td><td>' . $results[$c]['compression'] . ':1</td><td>' . $results[$c]['induction'] . '</td><td>' . $results[$c]['firmware'] . '/' . $results[$c]['signature'] . '</td><td>' . $results[$c]['uploadDate'] . '</td><td></td></tr>';
 }
-echo '</table>';
+echo '</table></div>';
+
+$msqur->footer();
 ?>
-</div>
--- src/db.php
+++ src/db.php
@@ -1,19 +1,82 @@
 <?php
 class MsqurDB
 {
+	private $db;
+	
+	private function connect()
+	{
+		if (isset($this->db) && $this->db instanceof PDO)
+		{
+			if (DEBUG) echo '<div class="debug">Reusing DB connection.</div>';
+		}
+		else
+		{
+			try
+			{
+				if (DEBUG) echo '<div class="debug">Connecting to DB: ' . "mysql:dbname=" . DB_NAME . ";host=" . DB_HOST . "," . DB_USERNAME . ", [****]" . '</div>';
+				$this->db = new PDO("mysql:dbname=" . DB_NAME . ";host=" . DB_HOST, DB_USERNAME, DB_PASSWORD);
+				//Persistent connection:
+				//$db = new PDO("mysql:dbname=" . DB_NAME . ";host=" . DB_HOST, DB_USERNAME, DB_PASSWORD, array(PDO::ATTR_PERSISTENT => true);
+			}
+			catch (PDOException $e)
+			{
+				echo '<div class="error">Error connecting to database.</div>';
+				$this->dbError($e);
+				$this->db = null; //Redundant.
+			}
+		}
+		
+		if (DEBUG) echo '<div class="debug">Connecting to DB: ' . (($this->db != null) ? 'Connected.' : 'Connection FAILED') . '</div>';
+		return ($this->db != null);
+	}
+	
+	function __construct()
+	{
+		$this->connect();
+	} 
+	
 	public function addMSQ() {}
 	public function addMSQs() {}
 	public function addEngine() {}
 	
 	public function getMSQ() {}
-	public function browse() {}
+	
+	public function browse($page)
+	{
+		if (!$this->connect()) return null;
+		
+		try
+		{
+			$st = $this->db->prepare("SELECT m.id as mid, numCylinders, displacement, compression, induction, firmware, signature, uploadDate FROM metadata m INNER JOIN engines e ON m.engine = e.id");
+			if ($st->execute())
+			{
+				$result = $st->fetchAll(PDO::FETCH_ASSOC);
+				return $result;
+			}
+			//TODO else throw error
+		}
+		catch (PDOException $e)
+		{
+			$this->dbError($e);
+		}
+		
+		return null;
+	}
 	
 	private function updateMSQ() {} //Cached HTML
 	private function updateEngine() {}
 	private function updateMetadata() {}
 	
-	private function connect() {}
-	private function dbError() {}
+	private function dbError($e)
+	{
+		if (DEBUG)
+		{
+			echo '<div class="error">Error executing database query. ';
+			echo $e->getMessage();
+			echo '</div>';
+		}
+	}
+	
 	private function getXML() {}
 }
 
