4d00515a967874fb3b4eb199a1b1af9d403e52c8
nearwood <nearwood@gmail.com>
2015-02-16 16:44:55 -0500

    update ignorey

--- src/config.php
+++ src/config.php
@@ -1,17 +1,12 @@
 <?php
-define('CONFIG_VERSION', "4");
+define('CONFIG_VERSION', "3");
 define('DB_HOST', "localhost");
 define('DB_USERNAME', "msqur");
-define('DB_PASSWORD', "hNqsmjMpmCbnfNBj");
+define('DB_PASSWORD', "d4jQTeCRRxuJeypD");
 define('DB_NAME', "msqur");
 
 define('DEBUG', TRUE);
 define('DISABLE_MSQ_CACHE', TRUE);
-
-error_reporting(E_ALL);
-
-ini_set('display_errors', DEBUG ? 'On' : 'Off');
-//ini_set('log_errors', DEBUG ? 'Off' : 'On');
 
 //Default in case it's not set in php.ini
 //MSQUR-1
