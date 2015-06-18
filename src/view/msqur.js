748c2b67e348fdd1e0c618c8597555259346b2bd
nearwood <nearwood@localhost>
2015-06-18 16:59:59 -0400

    Add GPL license.

--- src/view/msqur.js
+++ src/view/msqur.js
@@ -1,3 +1,19 @@
+/* msqur - MegaSquirt .msq file viewer web application
+Copyright (C) 2015 Nicholas Earwood nearwood@gmail.com http://nearwood.net
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
 "use strict";
 
 $(function() {
