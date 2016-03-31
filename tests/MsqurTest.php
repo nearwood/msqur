8f0457e277bfa7ecb0094aef5b65ad26ebba5286
Nick Earwood <nearwood@gmail.com>
2016-03-31 19:00:52 -0400

    add tests.

---
+++ tests/MsqurTest.php
@@ -0,0 +1,25 @@
+<?php
+
+//require "../src/msqur.php";
+
+
+class MsqurTest extends PHPUnit_Framework_TestCase
+{
+    // ...
+
+    public function testCanBeInstantiated()
+    {
+        // Arrange
+        //$msqur = new Msqur();
+
+        // Act
+        
+
+        // Assert
+        //$this->assertNotNull($msqur);
+        $this->assertNull(null);
+    }
+
+    // ...
+}
+?>
