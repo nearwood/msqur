ad8e5a09cd05cde8017586285b528b1b3eb5fbbd
Nick <nearwood@gmail.com>
2019-10-14 21:59:08 -0500

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ ssh-add .travis/deploy_key.pem
 
 #TODO Run backup script on remote
 
-rsync -av --delete --exclude='*/.git*:.svn*:*/config.php' src/ $DEPLOY_HOST/
+rsync -av --delete --exclude='*/.git*:.svn*:*/config.php' -e "ssh -o StrictHostKeyChecking=no" src/ $DEPLOY_HOST/
 
 rm .travis/deploy_key.pem
 #cp -v VERSION $DEPLOY_DIR/ > /dev/null 2>&1
