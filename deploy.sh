2f92acc5cab0c38f109f6bd05c6adce8483c9c32
Nick <nearwood@gmail.com>
2019-10-14 22:51:33 -0500

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #. script.config
 
 eval "$(ssh-agent -s)"
-#chmod 600 .travis/id_rsa
-#ssh-add .travis/id_rsa
+chmod 600 ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa
 
 #TODO Run backup script on remote
 
 rsync -av --delete --exclude='*/.git*:.svn*:*/config.php' -e "ssh -o StrictHostKeyChecking=no" src/ $DEPLOY_HOST/
 
-#rm .travis/id_rsa
+rm ~/.ssh/id_rsa
 #cp -v VERSION $DEPLOY_DIR/ > /dev/null 2>&1
