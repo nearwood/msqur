ccce682c51c3992c4b0dc1db29cf193f16d9f72f
Nick <nearwood@gmail.com>
2019-10-14 22:57:19 -0500

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ ssh-add .travis/id_rsa
 
 #TODO Run backup script on remote
 
-rsync -av --delete --exclude='*/.git*:.svn*:*/config.php' -e "ssh -o StrictHostKeyChecking=no" src/ $DEPLOY_HOST/
+rsync -av --delete --exclude='*/.git*:.svn*:config.php:.well-known' -e "ssh -o StrictHostKeyChecking=no" src/ $DEPLOY_HOST/
 
 rm .travis/id_rsa
 #cp -v VERSION $DEPLOY_DIR/ > /dev/null 2>&1
