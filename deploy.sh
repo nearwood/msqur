d303e703d0dad215aa852801cc5af6e83e4e20b6
Nick <nearwood@gmail.com>
2019-10-14 21:10:49 -0500

    redo travis CI

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
-. script.config
+#. script.config
 
-rsync -av --delete --exclude='*/.git*:.svn*' src/ $DEPLOY_DIR/
-cp -v VERSION $DEPLOY_DIR/ > /dev/null 2>&1
+eval "$(ssh-agent -s)"
+chmod 600 .travis/deploy_key.pem
+ssh-add .travis/deploy_key.pem
+
+#TODO Run backup script on remote
+
+rsync -av --delete --exclude='*/.git*:.svn*:*/config.php' src/ $DEPLOY_DIR/
+
+rm .travis/deploy_key.pem
+#cp -v VERSION $DEPLOY_DIR/ > /dev/null 2>&1
