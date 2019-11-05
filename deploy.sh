3137181b1ea609cede9fbc2168823d8e7319bbff
Nick <nearwood@gmail.com>
2019-11-05 10:21:09 -0600

    add version string to deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,8 @@ ssh-add .travis/id_rsa
 
 #TODO Run backup script on remote
 
+git rev-parse HEAD | cut -b -7 > src/VERSION
+
 rsync -av --delete --exclude-from 'deploy_excludes.txt' -e "ssh -o StrictHostKeyChecking=no" src/ $DEPLOY_HOST/
 
 rm .travis/id_rsa
-#cp -v VERSION $DEPLOY_DIR/ > /dev/null 2>&1
